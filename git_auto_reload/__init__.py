7cb4b749b7b859dfa2ea714d9551aa1c1533bf66
木之清泠 <clemooling@staringplanet.top>
2026-03-16 00:10:01 +0800

    fix: correct command

--- git_auto_reload/__init__.py
+++ git_auto_reload/__init__.py
@@ -1,9 +1,11 @@
 from mcdreforged.api.all import PluginServerInterface
+from git_auto_reload.command import register
 
 
 def on_load(s: PluginServerInterface, _):
+    register(s)
     s.logger.info("PluginDevHelper loaded.")
 
 
-def on_unload(s: PluginServerInterface, _):
+def on_unload(s: PluginServerInterface):
     s.logger.info("PluginDevHelper unloaded.")
--- git_auto_reload/command.py
+++ git_auto_reload/command.py
@@ -6,10 +6,11 @@ from mcdreforged.api.all import (
 )
 
 
-main_command = Literal(["!!pdh", "!!plugin_dev_helper"]).runs(
+main_command = Literal(["!!gar", "!!git_auto_reload"]).runs(
     lambda src: src.reply("Main command.")
 )
 
 
 def register(s: PluginServerInterface):
     s.register_command(main_command)
+
